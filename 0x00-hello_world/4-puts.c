7dd62fb0fdad701c10709cfadf7c5fe6f4e5ed1f
0marleves <0marleves99@gmail.com>
2023-03-16 07:10:11 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ * main - Entry poin
+*/
 int main(void)
 {
  	puts("\"Programming is like building a multilingual puzzle");
