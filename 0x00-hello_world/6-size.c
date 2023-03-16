6e72d4d4f9775ed1089dd3dbb2ff9c15c2b92f9d
0marleves <0marleves99@gmail.com>
2023-03-16 07:51:44 -0700

    test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
-int main() {
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main() 
+{
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
